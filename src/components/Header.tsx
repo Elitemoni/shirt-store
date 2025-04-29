60443802052aff9f601d35733881e0648a81d3a8
Elitemoni <cekovsimeon1@gmail.com>
2025-04-28 19:15:27 -0700

    no image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,13 @@ import Image from 'next/image'
 export default function Header() {
    return (
       <div>
-         <Image
+         {/* <Image
             src="public/shirt_logo.jpeg"
             alt="Logo"
             width={100}
             height={100}
             className="absolute top-0 left-0 z-10"  
-         />
+         /> */}
          <button>Store</button>
          <button>Cart</button>
          <button>Sign-In</button>
