457f570772ca8ef5d719f38bab726659e8df5ba2
Elitemoni <cekovsimeon1@gmail.com>
2025-04-28 19:31:01 -0700

    try to vercel connect

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 export default function Header() {
    return (
-      <div>
+      <div className="flex justify-center items-center bg-gray-800 text-black font-bold black-white h-16">
          {/* <Image
             src="public/shirt_logo.jpeg"
             alt="Logo"
@@ -11,9 +11,9 @@ export default function Header() {
             height={100}
             className="absolute top-0 left-0 z-10"  
          /> */}
-         <button>Store</button>
-         <button>Cart</button>
-         <button>Sign-In</button>
+         <button className="flex-2 bg-blue-500 h-full">Store</button>
+         <button className="flex-1 bg-amber-300 h-full">Cart</button>
+         <button className="flex-1 bg-white h-full">Sign-In</button>
       </div>
    )
 }
